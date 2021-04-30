57d1b976dff7249b3ebe00b3a00d251026eb5f0b
Danil Pismenny <danil@brandymint.ru>
2021-04-30 09:58:32 +0300

    minor

--- lib/gera/engine.rb
+++ lib/gera/engine.rb
@@ -8,7 +8,6 @@ module Gera
     # Идея отсюда - https://github.com/thoughtbot/factory_bot_rails/pull/149/files?diff=split&short_path=04c6e90
     #
     initializer 'gera.factories', after: 'factory_bot.set_factory_paths' do
-      puts 'gera.factories'
       FactoryBot.definition_file_paths << File.expand_path('../../../factories', __FILE__) if defined?(FactoryBot)
     end
 
