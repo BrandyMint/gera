e765b5494d328906f9d255d262fc268f5abf036f
Danil Pismenny <danil@brandymint.ru>
2021-05-24 09:11:53 +0300

    Use action scope to i18n

--- app/decorators/gera/payment_system_decorator.rb
+++ app/decorators/gera/payment_system_decorator.rb
@@ -9,7 +9,7 @@ module Gera
     end
 
     def actions
-      h.link_to t('.edit'), h.edit_payment_system_path(object), class: 'btn btn-outline btn-default btn-sm' if object.updatable_by? current_user
+      h.link_to t('actions.edit'), h.edit_payment_system_path(object), class: 'btn btn-sm' if object.updatable_by? current_user
     end
 
     def self.decorated_class
